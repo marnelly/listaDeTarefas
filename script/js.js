bd2c77cc5f217be458e6d9ce3d077bca2446543f
marnelly <marnellysabrino16@gmail.com>
2023-03-07 01:49:36 -0300

    first commit

---
+++ script/js.js
@@ -0,0 +1,48 @@
+const tarefa = document.querySelector('#inpTarefa')
+const btnTarefa = document.querySelector('.btnTarefa')
+const ulTarefa = document.querySelector('.ulTarefas')
+const div = document.querySelector('.container')
+
+function criaLi() {
+    const li = document.createElement('li')
+    return li
+}
+
+btnTarefa.addEventListener('click' , function() {
+    if (tarefa.value === '') return;
+    criaTarefa()
+})
+
+function limpaInput() {
+    tarefa.value = ''
+    tarefa.focus()
+}
+
+div.addEventListener('keypress' , function(e) {
+    if (e.key === 'Enter') {
+        criaTarefa()
+    }
+})
+
+function criaTarefa() {
+    let li = criaLi()
+    li.innerHTML = tarefa.value
+    ulTarefa.appendChild(li)
+    criaBtnApagar(li)
+    limpaInput()
+}
+
+function criaBtnApagar(li) {
+    li.innerHTML += ' '
+    let btnApagar = document.createElement('button')
+    btnApagar.innerHTML = 'Apagar'
+    btnApagar.setAttribute('class', 'apagar')
+    li.appendChild(btnApagar)
+}
+
+document.addEventListener('click' , function (e) {
+    let click = e.target
+    if (click.classList.contains('apagar')){
+        click.parentElement.remove()
+    }
+})
